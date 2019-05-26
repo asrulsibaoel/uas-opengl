f49e958ff6eba09ac194be0ce57fba81c7941c24
asrulsibaoel <asrul@kata.ai>
2019-05-26 10:44:40 +0700

    Added: express

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/js/:fileName',function(req,res){
+    const { fileName } = req.params;
+    res.sendFile(path.join(__dirname+'/js/' + fileName));
+    //__dirname : It will resolve to your project folder.
+});
+
+const port = 8080;
+app.use('/', router);
+app.listen(process.env.port || port);
+
+console.log(`Running at Port ${port}`);
