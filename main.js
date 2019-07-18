ca87fea2bbafac69a75029217cb21a4bae174fd7
asrulsibaoel <asrul@kata.ai>
2019-07-18 20:04:31 +0700

    Added: router for assets

--- main.js
+++ main.js
@@ -14,6 +14,12 @@ router.get('/js/:fileName',function(req,res){
     //__dirname : It will resolve to your project folder.
 });
 
+router.get('/assets/:fileName',function(req,res){
+    const { fileName } = req.params;
+    res.sendFile(path.join(__dirname+'/js/' + fileName));
+    //__dirname : It will resolve to your project folder.
+});
+
 const port = 8080;
 app.use('/', router);
 app.listen(process.env.port || port);
