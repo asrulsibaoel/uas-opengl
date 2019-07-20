b1ac678caa2e5f8296d3dab213da91c04ab18c54
asrulsibaoel <asrul@kata.ai>
2019-07-20 15:44:45 +0700

    Finished: Mesh

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
 });
+router.get('/main2',function(req,res){
+    res.sendFile(path.join(__dirname+'/index2.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
 router.get('/js/:fileName',function(req,res){
     const { fileName } = req.params;
